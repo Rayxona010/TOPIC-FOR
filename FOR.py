96a1672a80b3fc9ee354007978256d9ecd88f9eb
Durdiyeva Rayxona <durdiyevarayxona60@gmail.com>
2025-11-17 18:19:18 +0500

    this topic is about  for. If you need it you can use it

---
+++ FOR.py
@@ -0,0 +1,41 @@
+# dostlar = ["R", "G", "B"]
+# ENG_YAQIN = ["R"]
+# for dost in dostlar:
+#     if dost in ENG_YAQIN:
+#         print(dost)
+#     else:
+#         print(f"dostim")
+#
+#
+#
+# dostlar = ["A", "V", "X", "H", "B", "A", "D", "J", "S", "M"]
+#
+# for dost in dostlar:
+#     print(f"Salom, {dost}! Qalaysan?")
+#
+#
+# dostlar = ["A", "V", "R", "H", "B", "A", "D", "J", "S", "M"]
+#
+# for dost in dostlar:
+#     if dost == "A":
+#         print("Salom, A! Bugun yaxshi kayfiyatdasanmi?")
+#     elif dost == "V":
+#         print("Hey V! Ishlaring qalay?")
+#     elif dost == "R":
+#         print("Assalomu alaykum, Hasan! Qandaysiz?")
+#     elif dost == "H":
+#         print("H, salom! Yangi yangiliklar bormi?")
+#     elif dost == "B":
+#         print("B, salom! Ko‘rinmay ketding-ku!")
+#     elif dost == "A":
+#         print("A aka, yaxshimisiz?")
+#     elif dost == "D":
+#         print("D, salom! Kayfiyat zo‘rmi?")
+#     elif dost == "J":
+#         print("J, hayrli kun! Ishlaring yaxshimi?")
+#     elif dost == "S":
+#         print("Salom, S! Hammasi joyidami?")
+#     elif dost == "M":
+#         print("M, do‘stim, salom! Nima gaplar?")
+#     else:
+#         print(f"Salom, {dost}!")
